7c9c83f4081ce7e95eab4a21568ae2417dcc5fb5
Tristin <TCturtle16@outlook.com>
2025-12-08 15:12:54 -0700

    Fixed my Main issue to now loading the scene

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -9,17 +9,43 @@
 #include "Sphere.h"
 #include "Random.h"
 
-
 int main() {
 	constexpr int SCREEN_WIDTH = 800;
 	constexpr int SCREEN_HEIGHT = 600;
 
+
 	// create renderer
 	Renderer renderer;
+	Scene scene;
 	renderer.Initialize();
 	renderer.CreateWindow("Ray Tracer", SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	Framebuffer framebuffer(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	float aspectRatio = (float)framebuffer.width / (float)framebuffer.height;
+	Camera camera(70.0f, aspectRatio);
+	camera.SetView({ 0, 0, 5 }, { 0, 0, 0 });
+	scene.SetSky({ 1.0f, 0.4f, 0.3f }, { 0.1f, 0.2f, 0.8f });
+
+	auto sphere = std::make_unique<Sphere>(
+		glm::vec3{ 0, 0, 0 },
+		2.0f,
+		color3_t{ 1, 0, 0 }
+	);
+	scene.AddObject(std::move(sphere));
+
+
+	for (int i = 0; i < 5; i++) {
+		glm::vec3 position = random::getReal(glm::vec3{ -3.0f }, glm::vec3{ 3.0f });
+		auto sphere = std::make_unique<Sphere>(
+			position,
+			1.0f,
+			color3_t{ 1, 0, 0 }
+		);
+
+		scene.AddObject(std::move(sphere));
+	}
+
 
 	SDL_Event event;
 	bool quit = false;
@@ -39,45 +65,12 @@ int main() {
 		// draw to frame buffer
 		framebuffer.Clear({ 0, 0, 0, 255 });
 		/*for (int i = 0; i < 300; i++) framebuffer.DrawPoint(rand() % SCREEN_WIDTH, rand() % SCREEN_HEIGHT, { 255, 255, 255, 255 });*/
-		
-
+		scene.Render(framebuffer, camera);
 		// update frame buffer, copy buffer pixels to texture
 		framebuffer.Update();
 
 		// copy frame buffer texture to renderer to display
 		renderer.CopyFramebuffer(framebuffer);
 		renderer.Show();
-
-		Framebuffer framebuffer(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-		float aspectRatio = (float)framebuffer.width / (float)framebuffer.height;//framebuffer width divided by framebuffer height (float division)
-		Camera camera(70.0f, aspectRatio);
-		camera.SetView({ 0, 0, 5 }, { 0, 0, 0 });
-
-		Scene scene; // after camera creation/initialization
-
-		scene.SetSky({ 1.0f, 0.4f, 0.3f }, { 0.1f, 0.2f, 0.8f });
-
-		auto sphere = std::make_unique<Sphere>(
-			glm::vec3{ 0, 0, 0 },
-			2.0f,
-			color3_t{ 1, 0, 0 }
-		);
-		scene.AddObject(std::move(sphere));
-
-
-		for (int i = 0; i < 5; i++) {
-			glm::vec3 position = random::getReal(glm::vec3{ -3.0f }, glm::vec3{ 3.0f });
-			auto sphere = std::make_unique<Sphere>(
-				position,
-				1.0f,
-				color3_t{ 1, 0, 0 }
-			);
-
-			scene.AddObject(std::move(sphere));
-		}
-
-		scene.Render(framebuffer, camera);
-
 	}
 }
